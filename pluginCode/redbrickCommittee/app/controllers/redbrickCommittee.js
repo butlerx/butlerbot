8e623e54acb568dabec83786c35b8b5449f95d31
butlerx <butlerx@notthe.cloud>
2017-08-17 18:02:26 +0100

    remove duplicate code

--- pluginCode/redbrickCommittee/app/controllers/redbrickCommittee.js
+++ pluginCode/redbrickCommittee/app/controllers/redbrickCommittee.js
@@ -7,142 +7,88 @@ const env = process.env.NODE_ENV || 'development';
 class RedbrickCommittee {
   constructor() {
     this.config = config[env];
+    this.postions = config.position;
   }
 
   committee() {
-    return new Promise((resolve, reject) => {
-      request({
-        uri    : this.config.url,
-        headers: {
-          'User-Agent': 'Request-Promise',
-        },
-        json: true,
-      })
-        .then(cmt => resolve(cmt))
-        .catch(error => reject(error));
+    return request({
+      uri    : this.config.url,
+      headers: {
+        'User-Agent': 'Request-Promise',
+      },
+      json: true,
     });
   }
 
-  showCommitteeInfo(client, message, cmdArgs) {
-    client.say(message.args[0], 'Committee details sent. Who you want to tell to resign!');
-    this.showChair(client, message, cmdArgs);
-    this.showSecretary(client, message, cmdArgs);
-    this.showTreasurer(client, message, cmdArgs);
-    this.showPRO(client, message, cmdArgs);
-    this.showEvents(client, message, cmdArgs);
-    this.showFYR(client, message, cmdArgs);
-    this.showWebmaster(client, message, cmdArgs);
-    this.showHelpdesk(client, message, cmdArgs);
-    this.showAdmins(client, message, cmdArgs);
-  }
-
-  async showChair(client, { nick }) {
-    const cmt = await this.committee();
-    const chairperson = _.find(cmt, { position: 'Chairperson' });
-    if (!_.isUndefined(chairperson) && this.chair) {
-      const chairString = `${chairperson.name} (${chairperson.nick})`;
-      client.say(
-        nick,
-        `Chairperson: ${chairString} contact by /m ${chairperson.nick} <message>, or email ${chairperson.name}@redbrick.dcu.ie`,
-      );
-      this.wait('Chair');
+  async showCommitteeInfo(client, { nick, args }) {
+    client.say(args[0], 'Committee details sent. So you can tell them to resign!');
+    try {
+      const cmt = await this.committee();
+      this.postions.forEach(position => this.show(client, nick, position, cmt));
+    } catch (err) {
+      console.error(err);
     }
   }
 
-  async showSecretary(client, { nick }) {
-    const cmt = await this.committee();
-    const secretary = _.find(cmt, { position: 'Secretary' });
-    if (!_.isUndefined(secretary) && this.sec) {
-      const secretaryString = `${secretary.name} (${secretary.nick})`;
-      client.say(
-        nick,
-        `Secretary: ${secretaryString} contact by /m ${secretary.nick} <message>, or email ${secretary.nick}@redbrick.dcu.ie`,
-      );
-      this.wait('Sec');
+  async show(client, nick, position, cmtArg) {
+    try {
+      const cmt = _.isUndefined(cmtArg) ? await this.committee() : cmtArg;
+      const memebers = _.filter(cmt, { position });
+      if (!_.isUndefined(memebers)) {
+        memebers.forEach(memeber => {
+          client.say(
+            nick,
+            `${position}: ${memeber.name} (${memeber.nick}) contact by /m ${memeber.nick} <message>, or email ${memeber.name}@redbrick.dcu.ie`,
+          );
+        });
+      }
+    } catch (err) {
+      console.error(err);
     }
   }
-
-  async showTreasurer(client, { nick }) {
-    const cmt = await this.committee();
-    const treasurer = _.find(cmt, { position: 'Treasurer' });
-    if (!_.isUndefined(treasurer) && this.treasurer) {
-      const treasurerString = `${treasurer.name} (${treasurer.nick})`;
-      client.say(
-        nick,
-        `Treasurer: ${treasurerString} contact by /m ${treasurer.nick} <message>, or email ${treasurer.nick}@redbrick.dcu.ie`,
-      );
-      this.wait('Treasurer');
-    }
+  showChair(client, { nick, args }) {
+    client.say(args[0], 'Chairperson details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Chairperson');
   }
 
-  async showPRO(client, { nick }) {
-    const cmt = await this.committee();
-    const pro = _.find(cmt, { position: 'Public Relations Officer' });
-    if (!_.isUndefined(pro) && this.pro) {
-      const proString = `${pro.name} (${pro.nick})`;
-      client.say(
-        nick,
-        `Public Relations Officer: ${proString} contact by /m ${pro.nick} <message>, or email ${pro.nick}@redbrick.dcu.ie`,
-      );
-      this.wait('PRO');
-    }
+  showSecretary(client, { nick, args }) {
+    client.say(args[0], 'Secretary details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Secretary');
   }
 
-  async showEvents(client, { nick }) {
-    const cmt = await this.committee();
-    const events = _.find(cmt, { position: 'Events Officer' });
-    if (!_.isUndefined(events) && this.events) {
-      const eventsString = `${events.name} (${events.nick})`;
-      client.say(
-        nick,
-        `Events Officer: ${eventsString} contact by /m ${events.nick} <message>, or email ${events.nick}@redbrick.dcu.ie`,
-      );
-    }
+  showTreasurer(client, { nick, args }) {
+    client.say(args[0], 'Treasurer details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Treasurer');
   }
 
-  async showFYR(client, { nick }) {
-    const cmt = await this.committee();
-    const firstYearRep = _.find(cmt, { position: 'First Year Representative' });
-    if (!_.isUndefined(firstYearRep) && this.fyr) {
-      const fyrString = `${firstYearRep.name} (${firstYearRep.nick})`;
-      client.say(
-        nick,
-        `First Year Representative: ${fyrString} contact by /m ${firstYearRep.nick} <message>, or email ${firstYearRep.nick}@redbrick.dcu.ie`,
-      );
-    }
+  showPRO(client, { nick, args }) {
+    client.say(args[0], 'PRO details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Public Relations Officer');
   }
 
-  async showWebmaster(client, { nick }) {
-    const cmt = await this.committee();
-    const webmaster = _.find(cmt, { position: 'Webmaster' });
-    if (!_.isUndefined(webmaster) && this.web) {
-      const webmasterString = `${webmaster.name} (${webmaster.nick})`;
-      client.say(
-        nick,
-        `Webmaster: ${webmasterString} contact by /m ${webmaster.nick} <message>, or email ${webmaster.nick}@redbrick.dcu.ie`,
-      );
-    }
+  showEvents(client, { nick, args }) {
+    client.say(args[0], 'Events details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Events Officer');
   }
 
-  async showHelpdesk(client, { nick }) {
-    const cmt = await this.committee();
-    const helpdesk = _.filter(cmt, { position: 'Helpdesk' });
-    if (!_.isUndefined(helpdesk) && this.helpdesk) {
-      const helpdeskString = _.map(helpdesk, ({ name, nick }) => `${name} (${nick})`).join(', ');
-      client.say(nick, `Helpdesk: ${helpdeskString} contact by emailing helpdesk@redbrick.dcu.ie`);
-    }
+  showFYR(client, { nick, args }) {
+    client.say(args[0], 'First year Reps details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'First Year Representative');
   }
 
-  async showAdmins(client, { nick }) {
-    const cmt = await this.committee();
-    const admins = _.filter(cmt, { position: 'System Administrator' });
-    if (!_.isUndefined(admins) && this.admins) {
-      const adminsString = _.map(admins, ({ name, nick }) => `${name} (${nick})`).join(', ');
-      client.say(
-        nick,
-        `System Administrators: ${adminsString} contact by emailing admins@redbrick.dcu.ie`,
-      );
-    }
+  showWebmaster(client, { nick, args }) {
+    client.say(args[0], 'Webmaster details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Webmaster');
+  }
+
+  showHelpdesk(client, { nick, args }) {
+    client.say(args[0], 'Helpdesk details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'Helpdesk');
+  }
+
+  showAdmins(client, { nick, args }) {
+    client.say(args[0], 'Admins details sent. So you can tell them to resign!');
+    return this.show(client, nick, 'System Administrator');
   }
 }
 
